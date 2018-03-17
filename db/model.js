cd2f5c81b1e6b9ce8c91c596997fba4005ce2cda
mohan <mohankukreja1@gmail.com>
2018-03-17 12:58:48 +0530

    db initialized
    Signed-off-by: mohan <mohankukreja1@gmail.com>

---
+++ db/model.js
@@ -0,0 +1,29 @@
+const Sequelize = require('sequelize');
+const datatypes=Sequelize.DataTypes
+
+const db=new Sequelize('splitwizedb','splituser','splitpass',{
+    host: 'localhost',
+    dialect:'mysql'
+})
+
+
+const splittable=db.define('splittable',{
+    id: {
+        type: datatypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    name: datatypes.STRING,
+    email:datatypes.STRING,
+    moneytogive:datatypes.INTEGER,
+    moneytotake:datatypes.INTEGER
+})
+
+
+db.sync({force: true})
+    .then(() => console.info("Database configured"))
+    .catch((err) => console.error(err))
+
+exports.models = {
+   splittable
+}
