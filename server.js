2b7879d50ecf592a7310bc3210bef8fe89979598
mohan <mohankukreja1@gmail.com>
2018-03-17 11:28:57 +0530

    heading initialized
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express=require('express');
+const path=require('path');
+const bodyParser=require('body-parser')
+
+
 
 const app=express();
 
-app.get('/',(req,res)=>{
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/hello',(req,res)=>{
     res.send('hello');
 })
+
+app.use('/',express.static(path.join(__dirname,'public_static')));
 
 app.listen(2222,()=>{
     console.log('server started');
