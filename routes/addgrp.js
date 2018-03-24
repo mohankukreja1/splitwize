911e5e156d024bf1d5e38fba6bcdab08dcd2b28d
mohan <mohankukreja1@gmail.com>
2018-03-24 18:15:34 +0530

    option added
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- routes/addgrp.js
+++ routes/addgrp.js
@@ -1,7 +1,37 @@
 const route = require('express').Router();
+const table=require('../db/model').grptable;
 
+var money;
 route.post('/',(req,res)=>{
-    console.log(req);
+
+    if(req.body.exampleRadios1 == 'option1'){
+       money = Number(req.body.money);
+       var divide=Number(req.body.number);
+       money=money/divide;
+       console.log(money);
+        table.update(
+            {
+                moneytogive:money,
+                moneytotake:0
+            },
+            { where: { id: 1 } }
+        ).then((res)=>{
+            table.findAll({}).then((res)=>{
+                console.log(res);
+            })
+                .catch((err)=>{
+                    console.log(err);
+                })
+
+        }).catch((err)=>{
+            console.log(err);
+        })
+
+
+
+
+    }
+
 })
 
 
