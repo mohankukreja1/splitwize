ab53ed8ecf2e20de5a8ef96df609a9ece5d8d97a
mohan <mohankukreja1@gmail.com>
2018-03-25 11:23:26 +0530

    corrections
    Signed-off-by: mohan <mohankukreja1@gmail.com>

--- routes/addgrp.js
+++ routes/addgrp.js
@@ -3,34 +3,123 @@ const table=require('../db/model').grptable;
 
 var money;
 route.post('/',(req,res)=>{
+    console.log(req);
 
     if(req.body.exampleRadios1 == 'option1'){
-       money = Number(req.body.money);
-       var divide=Number(req.body.number);
-       money=money/divide;
-       console.log(money);
-        table.update(
-            {
-                moneytogive:money,
-                moneytotake:0
-            },
-            { where: { id: 1 } }
-        ).then((res)=>{
-            table.findAll({}).then((res)=>{
-                console.log(res);
+        if(req.body.number=='1'){
+            money = Number(req.body.money);
+            var divide=Number(req.body.number);
+            money=money/divide;
+            console.log(money);
+            table.update(
+                {
+                    moneytogive:money,
+                    moneytotake:0
+                },
+                { where: { id: 1 } }
+            ).then((res)=>{
+                table.findAll({}).then((res)=>{
+                    console.log(res);
+                })
+                    .catch((err)=>{
+                        console.log(err);
+                    })
+
+            }).catch((err)=>{
+                console.log(err);
             })
-                .catch((err)=>{
+
+
+        }
+
+
+
+        if(req.body.number=='2'){
+
+            money = Number(req.body.money);
+            var divide=Number(req.body.number);
+            money=money/divide;
+            console.log(money);
+            table.update(
+                {
+                    moneytogive:0,
+                    moneytotake:money
+                },
+                { where: { id: 1 } }
+            ).then((res)=>{
+
+                table.update(
+                    {
+                        moneytogive:money,
+                        moneytotake:0
+                    },
+                    { where: { id: 2 } }
+                ).then((res)=>{
+                    table.findAll({}).then((res)=>{
+                        console.log(res);
+                    })
+                        .catch((err)=>{
+                            console.log(err);
+                        })
+
+
+                }).catch((err)=>{
                     console.log(err);
                 })
 
-        }).catch((err)=>{
-            console.log(err);
-        })
+
+            }).catch((err)=>{
+                console.log(err);
+            })
+
+
+
+        }
 
 
 
 
     }
+    if(req.body.exampleRadios2 == "option2"){
+        money = Number(req.body.money);
+        var divide=Number(req.body.number);
+        money=money/divide;
+        table.update(
+            {
+                moneytogive:0,
+                moneytotake:money
+            },
+            { where: { id: 2 } }
+        ).then((res)=>{
+
+            table.update(
+                {
+                    moneytogive:money,
+                    moneytotake:0
+                },
+                { where: { id: 1 } }
+            ).then((res)=>{
+                table.findAll({}).then((res)=>{
+                    console.log(res);
+                })
+                    .catch((err)=>{
+                        console.log(err);
+                    })
+
+            }).catch((err)=>{
+                console.log(err);
+            })
+
+
+
+
+
+
+        }).catch((err)=>{
+            console.log(err);
+        })
+    }
+
 
 })
 
